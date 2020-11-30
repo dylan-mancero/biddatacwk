5c83baa561c43f8ddd4722084d60e0e3f6d38ddb
Dylan Mancero <israelman@hotmail.co.uk>
2020-11-30 16:12:43 +0000

    fix

--- tdaycount.py
+++ tdaycount.py
@@ -1,25 +1,25 @@
 from mrjob.job import MRJob
 import time
 
-class Lab3part1(MRJob):
+class tdaycount(MRJob):
 
     def mapper(self, _, line):
         try:
             fields = line.split(',')
             if len(fields) == 7 :
-                time_epoch = int(fields[0])/1000
-                day = time.strftime("%d",time.gmtime(time_epoch)) #returns day of the month
-                yield (day, 1)
+                time_epoch = int(fields[6])
+                month = time.strftime("%B",time.gmtime(time_epoch)) 
+                yield (month, 1)
         except:
             pass
             #do nothing
 
-    def combiner(self, day, counts):
-        yield (day, sum(counts))
+    def combiner(self, month, counts):
+        yield (month, sum(counts))
 
-    def reducer(self, day, counts):
-        yield (day, sum(counts))
+    def reducer(self, month, counts):
+        yield (month, sum(counts))
 
 
 if __name__ == '__main__':
-    Lab3part1.run()
+    tdaycount.run()
